4de435e3d7da150341be7539f8b840e3b66d3953
AthrvTech <mrhackershivansh143@gmail.com>
2025-08-01 10:35:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Simple contact form handler (shows an alert instead of sending an email)
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for reaching out! I will get back to you soon.');
-    this.reset();
-});
